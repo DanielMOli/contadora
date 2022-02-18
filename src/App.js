7c116c5c10d5dcc727ba0edcc3226c1f35fa9dba
“DanielMOli” <arayty0suave@gmail.com>
2022-02-18 14:03:02 -0300

    contadora do demônio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+ import React, { Component } from "react";
+ import ReactDom from "react-dom";
+ import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ class App extends Component {
+   constructor() {
+     super();
 
-export default App;
+     this.state = {
+       num: 0,
+     };
+  }
+    
+    clear = () => {
+         this.setState({
+           num: this.state.num * 0,
+         });
+    }
+    
+     sub = () => {
+         this.setState({
+           num: this.state.num -1,
+         });
+     }
+    
+    add = () => {
+        this.setState({
+          num: this.state.num + 1,
+        });
+     }
+    
+         render(){
+           let soma = '';
+           if(this.state.num < 10){
+            soma = <button onClick={this.add}>Somar</button>
+           }else{
+             soma = <button disabled onClick={this.add}>Somar</button>
+           }
+
+           let dim = '';
+           if(this.state.num > 0){
+            dim = <button onClick={this.sub}>Subtrair</button>
+           }else{
+             dim = <button disabled onClick={this.sub}>Subtrair</button>
+           }
+
+             return(
+                <div>
+                    <h1> Você clicou {this.state.num}</h1>
+                     {soma}
+                     {dim}
+                     <button onClick={this.clear}>Limpar</button>
+                 </div>
+                 )
+         }
+     }
+
+ export default App;
